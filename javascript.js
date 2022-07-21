8b9c19d46665fda70f592329d9b48b1bcb540ead
Youssef KERROUM <78832703+YKerroum@users.noreply.github.com>
2022-07-21 01:48:08 +0100

    Add email check function

--- javascript.js
+++ javascript.js
@@ -296,6 +296,15 @@ if(btnSubmitForm != null) {
   });
 }
 
+function checkForm(){
+const email = document.querySelector('#email_address').value;
+const lowEmail= email.toLowerCase();
+if(email === lowEmail) {
+return true;
+}
+else return false;
+}
+
 const isEmailValid = (email) => {
   const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
   console.log(re);
